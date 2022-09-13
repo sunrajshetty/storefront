4debce24f01b14bca3088a14f2e6242f57b9e241
Sunraj Shetty <sunrajshetty17@gmail.com>
2022-09-13 16:59:45 +0530

    Filtering objects

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,15 @@
+from genericpath import exists
+from itertools import product
+from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 
-def calculate():
-    x = 1
-    y = 2
-    return x
 
 
 def say_hello(request):
-    x = calculate()
-    return render(request, 'hello.html', {'name': 'Mosh'})
+    query_set = Product.objects.filter(description__isnull=True)
+
+    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(query_set)})
