8a5f857b93000c30e9e24966e5ab577bfec84ea0
Sunraj Shetty <sunrajshetty17@gmail.com>
2022-09-14 10:41:50 +0530

    Selecting fieldds to query

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,7 @@ from itertools import product
 from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product
+from store.models import Product, OrderItem
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
 
@@ -15,7 +15,12 @@ def say_hello(request):
     # product = Product.objects.earliest('unit_price')
     # product = Product.objects.latest('unit_price')
     # queryset = Product.objects.all()[:5]
-    queryset = Product.objects.all()[5:10]
+    # queryset = Product.objects.all()[5:10]
+    # queryset = Product.objects.values('id', 'title')
+    # queryset = Product.objects.values('id', 'title', 'collection__title')
+    # queryset = Product.objects.values_list('id', 'title', 'collection__title')
+    # queryset = OrderItem.objects.values('product_id').distinct()
+    queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
 
     return render(request, 'hello.html', {'name': 'Mosh', 'products': list(queryset)})
     # return render(request, 'hello.html', {'name': 'Mosh', 'product': product})
