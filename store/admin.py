94cd9a8471a230a712ba21af8ab7a4a760103c6a
Sunraj Shetty <sunrajshetty17@gmail.com>
2022-09-14 18:08:20 +0530

    Selecting related objects

--- store/admin.py
+++ store/admin.py
@@ -1,12 +1,18 @@
+from itertools import product
 from django.contrib import admin
 
 from store import models
 
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
     list_per_page = 10
+    list_select_related = ['collection']
+
+    def collection_title(self, product):
+        return product.collection.title
+
     @admin.display(ordering='inventory')
     def inventory_status(self, product):
         if product.inventory < 10:
@@ -21,5 +27,8 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
-
 # admin.site.register(models.Product, ProductAdmin)
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer']
