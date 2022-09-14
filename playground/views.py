97148f49438bc08ae637017328c1d2f0890cbd35
Sunraj Shetty <sunrajshetty17@gmail.com>
2022-09-14 10:03:32 +0530

    Limiting results

--- playground/views.py
+++ playground/views.py
@@ -13,7 +13,9 @@ def say_hello(request):
     # queryset = Product.objects.order_by('unit_price', '-title').reverse()
     # product = Product.objects.order_by('unit_price')[0]
     # product = Product.objects.earliest('unit_price')
-    product = Product.objects.latest('unit_price')
+    # product = Product.objects.latest('unit_price')
+    # queryset = Product.objects.all()[:5]
+    queryset = Product.objects.all()[5:10]
 
-    # return render(request, 'hello.html', {'name': 'Mosh', 'products': list(queryset)})
-    return render(request, 'hello.html', {'name': 'Mosh', 'product': product})
+    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(queryset)})
+    # return render(request, 'hello.html', {'name': 'Mosh', 'product': product})
