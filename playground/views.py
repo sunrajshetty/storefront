ffc18389b9b5df95f01658c3b1845b48b2e66e5a
Sunraj Shetty <sunrajshetty17@gmail.com>
2022-09-14 14:51:26 +0530

    Calling Database Functions

--- playground/views.py
+++ playground/views.py
@@ -6,7 +6,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Collection, Customer, Order, Product, OrderItem
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import Q, F
+from django.db.models import Q, F, Value, Func
+from django.db.models.functions import Concat
 from django.db.models.aggregates import Count, Max, Min, Avg, Sum
 
 
@@ -34,9 +35,14 @@ def say_hello(request):
     # queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
     # result = Product.objects.filter(collection__id=1).aggregate(count=Count('id'), min_price=Min('unit_price'))
     # result = Product.objects.aggregate(count=Count('id'), min_price=Min('unit_price'))
-    result = Product.objects.filter(collection__id=3).aggregate(min_price=Min('unit_price'), avg_price=Avg('unit_price'), max_price=Max('unit_price'))
+    # result = Product.objects.filter(collection__id=3).aggregate(min_price=Min('unit_price'), avg_price=Avg('unit_price'), max_price=Max('unit_price'))
+    # queryset = Customer.objects.annotate(is_new=Value(True))
+    # queryset = Customer.objects.annotate(new_id=F('id') + 1)
+    # queryset = Customer.objects.annotate(full_name=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT'))
+    queryset = Customer.objects.annotate(full_name=Concat('first_name', Value(' '), 'last_name'))
 
     # return render(request, 'hello.html', {'name': 'Mosh', 'products': queryset})
     # return render(request, 'hello.html', {'name': 'Mosh', 'orders': list(queryset)})
-    return render(request, 'hello.html', {'name': 'Mosh', 'result': result})
+    # return render(request, 'hello.html', {'name': 'Mosh', 'result': result})
+    return render(request, 'hello.html', {'name': 'Mosh', 'result': list(queryset)})
     # return render(request, 'hello.html', {'name': 'Mosh', 'product': product})
