94b1743e5a92ee80e69e448c2dc0008b26b0b3e2
Sunraj Shetty <sunrajshetty17@gmail.com>
2022-09-14 11:24:32 +0530

    Selecting related objects

--- playground/views.py
+++ playground/views.py
@@ -3,7 +3,7 @@ from itertools import product
 from turtle import title
 from django.shortcuts import render
 from django.http import HttpResponse
-from store.models import Product, OrderItem
+from store.models import Order, Product, OrderItem
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q, F
 
@@ -22,7 +22,14 @@ def say_hello(request):
     # queryset = OrderItem.objects.values('product_id').distinct()
     # queryset = Product.objects.filter(id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
     # queryset = Product.objects.only('id', 'title')
-    queryset = Product.objects.defer('description')
+    # queryset = Product.objects.defer('description')
+    # queryset = Product.objects.all()
+    # queryset = Product.objects.select_related('collection').all()
+    # queryset = Product.objects.select_related('collection__someOtherField').all()
+    # queryset = Product.objects.prefetch_related('promotions').all()
+    # queryset = Product.objects.prefetch_related('promotions').select_related('collection').all()
+    queryset = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
 
-    return render(request, 'hello.html', {'name': 'Mosh', 'products': list(queryset)})
+    # return render(request, 'hello.html', {'name': 'Mosh', 'products': list(queryset)})
+    return render(request, 'hello.html', {'name': 'Mosh', 'orders': list(queryset)})
     # return render(request, 'hello.html', {'name': 'Mosh', 'product': product})
