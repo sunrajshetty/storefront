56278aac2b8c3e384e506eaa5dbecae71e7368c1
Sunraj Shetty <sunrajshetty17@gmail.com>
2022-09-15 12:00:28 +0530

    Creating filtering to the list page

--- store/admin.py
+++ store/admin.py
@@ -1,13 +1,29 @@
+from wsgiref.handlers import format_date_time
 from django.contrib import admin
 from django.db.models import Count
 from store import models
 from django.utils.html import format_html, urlencode
 from django.urls import reverse
 
+
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
     list_editable = ['unit_price']
+    list_filter = ['collection', 'last_update', InventoryFilter]
     list_per_page = 10
     list_select_related = ['collection']
 
@@ -34,11 +50,20 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership']
+    list_display = ['first_name', 'last_name', 'membership', 'orders']
     list_editable = ['membership']
-    ordering = ['first_name', 'last_name']
     list_per_page = 10
+    ordering = ['first_name', 'last_name']
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
 # admin.site.register(models.Product, ProductAdmin)
+    @admin.display(ordering='orders_count')
+    def orders(self, customer):
+        url = (reverse('admin:store_order_changelist') + '?' + urlencode({'customer__id': str(customer.id)}))
+        return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
+        # return customer.orders
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(orders_count=Count('order'))
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
--- store/models.py
+++ store/models.py
@@ -52,7 +52,7 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
     def __str__(self) -> str:
-        return f'{self.first_name} {last_name}'
+        return f'{self.first_name} {self.last_name}'
     
     class Meta:
         ordering = ['first_name', 'last_name']
